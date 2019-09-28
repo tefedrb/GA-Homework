468d463ca804f92e5786b9d578da14cd8ff61ec9
tefedrb-enterprise <tefebell@gmail.com>
2019-09-28 01:41:44 -0400

    Found Carmen

--- wk5-sql-master/sql-intro-lab/starter-code/clues.sql
+++ wk5-sql-master/sql-intro-lab/starter-code/clues.sql
@@ -33,15 +33,18 @@ Country: San Marino;
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 SELECT * FROM city WHERE name LIKE 'Serra%';
-Country name = Serra;
+City name = Serra;
 Countrycode = BRA
 District = Espï¿½rito Santo
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+SELECT capital FROM country WHERE code='BRA';
+Capital = 211;
 
-
+SELECT id, name FROM city WHERE id='211';
+Name='Brasï¿½lia'
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -56,10 +59,12 @@ District = Espï¿½rito Santo
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
-
+SELECT * FROM city WHERE population=91084;
+name= Santa Monica
+countrycode = USA
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
--- She's in ____________________________!
+-- She's in Santa Monica, USA!
